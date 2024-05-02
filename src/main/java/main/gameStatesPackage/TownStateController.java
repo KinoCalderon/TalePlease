d8fd9e22960f0717955bf0791f5dba75f29cef34
titjug <120094613+titjug@users.noreply.github.com>
2024-05-02 03:44:47 -0400

    v.1

--- src/main/java/main/gameStatesPackage/TownStateController.java
+++ src/main/java/main/gameStatesPackage/TownStateController.java
@@ -1,9 +1,5 @@
 package main.gameStatesPackage;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public class TownStateController {
 
--- src/main/java/main/mainPackage/MainController.java
+++ src/main/java/main/mainPackage/MainController.java
@@ -13,22 +13,13 @@ import java.io.IOException;
 public class MainController {
 
     @FXML
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    @FXML
-
     public void gotoTown(ActionEvent e) throws IOException {
-
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/gameStatesPackage/townStateController.fxml"));
-        root = loader.load();
+        Parent root = loader.load();
 
-
-        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
 }
